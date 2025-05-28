feb7479733432d2d9cdd230729f31972a6fdf314
Virabyan99 <gmparstone99@gmail.com>
2025-05-28 19:46:08 +0400

    Module 3 Lesson 7 Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Geist, Geist_Mono, Inter, Roboto_Flex  } from 'next/font/google'
 import './globals.css'
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 })
 
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
+const roboto = Roboto_Flex({ subsets: ["latin"], variable: "--font-roboto" })
+
 export const metadata: Metadata = {
   title: { default: 'Markdown Viewer', template: '%s · Markdown Viewer' },
   description: 'Secure, client-side, drag-and-drop Markdown renderer.',
@@ -27,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${roboto.variable}`}>
       <body className="flex min-h-screen flex-col bg-background font-sans antialiased">
         <SkipToContent />
         <ThemeProvider>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,17 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  darkMode: "class", // Enables class-based dark mode
+  darkMode: "class",
   content: [
     "./app/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", "var(--font-roboto)", "Noto Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
